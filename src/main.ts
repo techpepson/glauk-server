03b7609b44040a8c08abb606e84a32a166fba07f
Dickson Daniel Peprah <techpepson@gmail.com>
2025-10-12 00:52:02 +0000

    init(project initialization): initialize project with installation of packages and project structure organization

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['log', 'debug', 'warn', 'fatal'],
+  });
+
+  //MVC
+  app.setBaseViewsDir(join(__dirname, 'views'));
+  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  //helmet for security
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
+
+  //enable cors
+  app.enableCors();
+
+  //set global prefix
+  app.setGlobalPrefix('api');
+
+  //listen to port
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
